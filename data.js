5431a299614abffab5be7d034b1b5aad31386431
Emmigumi <20160133@lamolina.edu.pe>
2022-03-27 20:14:41 -0500

    Updates

---
+++ data.js
@@ -0,0 +1,87 @@
+import data from "./data/ghibli/ghibli.js";
+
+//-----------Lógica de filtro para directores--------------------------------
+export const filterDataDirector = (films, nameDirector) => {
+  const resultDirector = films.filter((y) => y.director === nameDirector);
+  return resultDirector;
+};
+/* console.log(filterDataDirector(data.films, 'Hiroyuki Morita')); */
+
+//-----------Lógica de filtro para productores--------------------------------
+export const filterDataProducer = (films, filterProducer) => {
+  const resultProducer = films.filter((y) => y.producer === filterProducer);
+  return resultProducer;
+};
+/* console.log(filterDataProducer(data.films,'Toru Hara')); */
+
+//-----------Obtener new array People--------------------------------
+export const NewArrayPeople = [];
+for (let i = 0; i < data.films.length; i++) {
+  for (let j = 0; j < data.films[i].people.length; j++) {
+    NewArrayPeople.push(data.films[i].people[j]);
+  }
+}
+
+export const filterDataBySpecie = (NewArrayPeople, filterBySpecie) => {
+  const resultBySpecie = NewArrayPeople.filter((y) => y.specie === filterBySpecie);
+  return resultBySpecie;
+};
+/* console.log("aparecen 129personajesHumanos", filterDataBySpecie(NewArrayPeople,'Human')); */
+
+//-------------Usando Sort(), según el score-------------
+export const compareAscending = (a, b) => {
+  if (a.release_date > b.release_date) {
+    return 1;
+  } else {
+    return -1;
+  }
+};
+/* console.log("Holi",compareAscending("1999","2000")); */
+export const compareDescending = (a, b) => {
+  if (a.release_date > b.release_date) {
+    return -1;
+  } else {
+    return 1;
+  }
+};
+
+export const sortData = (order, films) => {
+  if (order === "Asc") {
+    const resultSort = films.sort(compareAscending);
+    return resultSort;
+  } /*  else if(order===""){
+
+  }  */ else {
+    const resultSort = films.sort(compareDescending);
+    return resultSort;
+  }
+};
+
+//---------------computeStats------------------------
+export const computeStats = (data, tipeOfspecie) => {
+  //data=datos sin orden   hace calculo en base a la data
+  let cantByEspecie = 0; // variable que indica cuantas veces tenemos tipoEspecie dentro de algun item de people
+  // dentro de cada film
+  for (let i = 0; i < data.length; i++) {
+    let People = data[i].people;
+    // dentro de cada people
+    for (let j = 0; j < People.length; j++) { //?Cambio
+      if (People[j].specie === tipeOfspecie) {
+        cantByEspecie++;
+      }
+    }
+  }
+  return cantByEspecie;
+};
+
+/* console.log(computeStats(data.films,"Demon")); */
+
+//* Ejemplo de cómo añadir una propiedad a nuestro objeto.
+
+/* const NewArrayPeople2 = [];
+data.films.forEach((pelicula) => {
+  pelicula.people.forEach((personaje) => {
+    personaje.movie=pelicula.title;
+    NewArrayPeople2.push(personaje);   //? Buscar objetos JS
+    console.log(personaje);
+  });*/
---
+++ main.js
@@ -0,0 +1,284 @@
+import data from './data/ghibli/ghibli.js';
+import { filterDataDirector, filterDataProducer, sortData, filterDataBySpecie, computeStats } from "./data.js";
+/* eslint-disable no-undef */
+
+
+
+//--------MOSTRAR Y OCULTAR--------
+const hiddenNav = document.getElementById("Menu");
+hiddenNav.classList.add("hidden");
+
+const hiddenFotter = document.getElementById("footer");
+hiddenFotter.classList.add("hidden");
+
+const showInicio = document.getElementById("inicio")
+showInicio.addEventListener("click", () => {
+    document.getElementById("onePage").style.display = "block";
+    document.getElementById("secondPage").style.display = "none";
+    document.getElementById("thirdPage").style.display = "none";
+    document.getElementById("fourthPage").style.display = "none";
+    hiddenFotter.classList.remove("hidden");
+    hiddenNav.classList.remove("hidden");
+});
+
+const showListMovie = document.getElementById("ListMovies")
+showListMovie.addEventListener("click", () => {
+    document.getElementById("onePage").style.display = "none";
+    document.getElementById("secondPage").style.display = "block";
+    document.getElementById("thirdPage").style.display = "none";
+    document.getElementById("fourthPage").style.display = "none";
+    hiddenFotter.classList.remove("hidden");
+    hiddenNav.classList.remove("hidden");
+});
+
+const show3Page = document.querySelector(".search-button")
+show3Page.addEventListener("click", () => {
+    document.getElementById("onePage").style.display = "block";
+    document.getElementById("secondPage").style.display = "none";
+    document.getElementById("thirdPage").style.display = "none";
+    document.getElementById("fourthPage").style.display = "none";
+    hiddenFotter.classList.remove("hidden");
+    hiddenNav.classList.remove("hidden");
+    alert("Working in page");
+});
+
+const show4Page = document.querySelector("#stadist")
+show4Page.addEventListener("click", () => {
+    document.getElementById("onePage").style.display = "none";
+    document.getElementById("secondPage").style.display = "none";
+    document.getElementById("thirdPage").style.display = "none";
+    document.getElementById("fourthPage").style.display = "block";
+    hiddenFotter.classList.remove("hidden");
+    hiddenNav.classList.remove("hidden");
+});
+
+// ---- ---- Variables para el carousel -------------------------------- ----
+const fila = document.querySelector('.contenedor-carousel');
+const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-derecha');
+
+// ---- ---- Event Listener para la flecha derecha ---- ----
+flechaDerecha.addEventListener('click', () => {
+    fila.scrollLeft += fila.offsetWidth;
+
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+    if (indicadorActivo.nextSibling) {
+        indicadorActivo.nextSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+// ---- ---- Event Listener para la flecha izquierda ---- ----
+flechaIzquierda.addEventListener('click', () => {
+    fila.scrollLeft -= fila.offsetWidth;
+
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+    if (indicadorActivo.previousSibling) {
+        indicadorActivo.previousSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+// ---- ---- Llamado del array para las imágenes en el carousel---- ----
+const carousel = document.querySelector("#carousel");
+const films = data.films;
+for (let i = 0; i < films.length; i++) {
+    const list = `
+    <div class="pelicula">
+    <a href="#">
+    <img src="${films[i].poster}" alt="">
+    </a>
+    </div>`;
+    carousel.insertAdjacentHTML("beforeend", list);
+}
+
+// ---- ---- Paginación ---- ----
+const peliculas = document.querySelectorAll('.pelicula');
+const numeroPaginas = Math.ceil(peliculas.length / 5);
+
+for (let i = 0; i < numeroPaginas; i++) {
+    const indicador = document.createElement('button');
+
+    if (i === 0) {
+        indicador.classList.add('activo');
+    }
+
+    document.querySelector('.indicadores').appendChild(indicador);
+    indicador.addEventListener('click', (e) => {
+        fila.scrollLeft = i * fila.offsetWidth;
+
+        document.querySelector('.indicadores .activo').classList.remove('activo');
+        e.target.classList.add('activo');
+    });
+}
+
+// ---- ---- Llamado del array para las imágenes de Movies ---- ----
+const moviesGrid = document.querySelector("#movies-grid");
+for (let i = 0; i < films.length; i++) {
+    const list = `
+    <div class="posterDescription">
+    <a href="#"><img src="${films[i].poster}" alt=""></a>
+    <div class="titleDescription"><label><b>${films[i].title}</b></label></div>
+    <div class="titleDescription"><label><b>(${films[i].release_date})</b></label></div>
+    </div>`;
+    moviesGrid.insertAdjacentHTML("beforeend", list);
+}
+
+export function filmsMoviesBy(films) {
+    moviesGrid.innerHTML = "";
+    for (let i = 0; i < films.length; i++) {
+        const list2 = `
+        <div class="posterDescription">
+        <a href="#"><img src="${films[i].poster}" alt=""></a>
+        <div class="titleDescription"><label><b>${films[i].title}</b></label></div>
+        <div class="titleDescription"><label><b>(${films[i].release_date})</b></label></div>
+        </div>`;
+        moviesGrid.insertAdjacentHTML("beforeend", list2);
+    }
+}
+
+// ---- ---- Acceder a los directores ---- ----
+const directors = document.querySelector("#filterDirector");
+const a = new Set([]);
+data.films.forEach(x => {
+    a.add(x.director);
+});
+
+const DataClean = [...a];
+
+for (let i = 0; i < DataClean.length; i++) {
+    const list = `
+    <option value="${DataClean[i]}">${DataClean[i]}</option>`;
+    directors.insertAdjacentHTML("beforeend", list);
+}
+directors.addEventListener('change', (e) => {
+    const selectedDirector = filterDataDirector(films, e.target.value);
+    filmsMoviesBy(selectedDirector);
+})
+
+// ---- ---- Acceder a los productores ---- ----
+const producers = document.querySelector("#filterProducer");
+const b = new Set([]);
+data.films.forEach(x => {
+    b.add(x.producer);
+});
+
+const DataClean2 = [...b];
+
+for (let i = 0; i < DataClean2.length; i++) {
+    const list = `
+    <option value="${DataClean2[i]}">${DataClean2[i]}</option>`;
+    producers.insertAdjacentHTML("beforeend", list);
+}
+producers.addEventListener('change', (e) => {
+    const selectedProducer = filterDataProducer(films, e.target.value);
+    filmsMoviesBy(selectedProducer);
+
+})
+
+//----------Ordenar con la funcion sortData-----------------------------------------
+const sectionOrderBox = document.getElementById("filterByOrder");
+sectionOrderBox.addEventListener('change', (e) => {
+    // console.log("change", e.target.value);
+    const orderSelected = sortData(e.target.value, films);
+    //console.log(orderSelected);
+    filmsMoviesBy(orderSelected);
+})
+
+//--------- Página 3 --------------------------------
+document.querySelectorAll('.accordion__button').forEach(button => {
+    button.addEventListener('click', () => {
+        const accordionContent = button.nextElementSibling;
+
+        button.classList.toggle('accordion__button--active');
+
+        if (button.classList.contains('accordion__button--active')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+        } else {
+            accordionContent.style.maxHeight = 0;
+        }
+    });
+});
+
+// --------- Función CompuTEst----------------------------------------------------
+//1.Llamado de array para las imagenes de personajes
+export const NewArrayPeople = [];
+for (let i = 0; i < data.films.length; i++) {
+    for (let j = 0; j < data.films[i].people.length; j++) {
+        NewArrayPeople.push(data.films[i].people[j]);
+    }
+}
+const Page4Grid = document.querySelector("#showAllPeople");
+for (let i = 0; i < NewArrayPeople.length; i++) {
+    const list = `
+    <div class="posterDescription">
+    <a href="#"><img src="${NewArrayPeople[i].img}" alt=""></a>
+    <div class="titleDescription"><label><b>${NewArrayPeople[i].name}</b></label></div>
+    </div>`;
+    Page4Grid.insertAdjacentHTML("beforeend", list);
+}
+//2. funcion de limpieza y filtro--------------
+function filmsPeopleBy(NewArrayPeople) {
+    Page4Grid.innerHTML = "";
+    for (let i = 0; i < NewArrayPeople.length; i++) {
+        const list2 = `
+        <div class="posterDescription">
+        <a href="#"><img src="${NewArrayPeople[i].img}" alt=""></a>
+        <div class="titleDescription"><label><b>${NewArrayPeople[i].name}</b></label></div>
+        <div><label><b>Edad: ${NewArrayPeople[i].age} años</b></label></div>
+        </div>`;
+        Page4Grid.insertAdjacentHTML("beforeend", list2);
+    }
+}
+//--------Acceder a las especies--------------------------------
+const BoxpeopleSpecie = document.querySelector("#filterSpecie");
+const c = new Set([]);
+NewArrayPeople.forEach(x => {
+    c.add(x.specie);
+});
+
+const DataClean3 = [...c];
+
+for (let i = 0; i < DataClean3.length; i++) {
+    const list = `
+    <option value="${DataClean3[i]}">${DataClean3[i]}</option>`;
+    BoxpeopleSpecie.insertAdjacentHTML("beforeend", list);
+}
+BoxpeopleSpecie.addEventListener('change', (e) => {
+    const selectedSpecie = filterDataBySpecie(NewArrayPeople, e.target.value);
+    const CantidadDeEspecie = computeStats(data.films, e.target.value);
+    const messageCantidadDeEspecie = `<div><label>El número de personajes es: ${CantidadDeEspecie}</label></div>`;
+    const BoxMessageCDEspecie = document.getElementById("NumberOfSpecies");
+    BoxMessageCDEspecie.innerHTML = messageCantidadDeEspecie;
+    filmsPeopleBy(selectedSpecie);
+})
+
+
+//Esto nos permite insertar información directa, manteniendo la semántica requerida en html, desde la data ghibli.
+// Permitiendonos mostrarlo en la interfaz.
+
+//-----Muestra la descripción de las movies------
+/* const descriptionAllMovies=document.querySelector("#AllMovie");
+
+for (let i=0; i<films.length; i++) {
+    const list = `
+ <li><img src="${films[i].poster}" alt=""></li>
+    <li>"${films[i].title}"</li>
+    <li>"${films[i].description}"</li>
+    <li>"${films[i].director}"</li>
+    <li>"${films[i].producer}"</li>
+    <li>"${films[i].release_date}"</li>
+    <li>"${films[i].rt_score}"</li>`;
+descriptionAllMovies.insertAdjacentHTML("beforeend", list);
+} */
+//TODO--------Search: Próxima funcionalidad--------------------------------
+//-----funcionalidad al search-------------
+/*  function filterItems(query) {
+    return data.filter(function(el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    })
+} */
+
+//ME PERMITE VER EL ARRAY COMPLETO EN CONSOLA
+/* const x= dataLovers.filterData(data, "Castle in the Sky");
+console.log(x); */
